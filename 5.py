578dde01dd0e66ee70e3a2e7fc760b82cabb4406
Aayush Kucheria <aayush.kucheria@gmail.com>
2019-03-20 17:24:57 +0530

    Working on 5

--- 5.py
+++ 5.py
@@ -1,39 +1,41 @@
-20, 19, 18, 17, 16, 15, 14, 13, 12, 11,
+import math
+
+9, 9, 7, 6, 5, 4, 14, 13, 12, 11,
 
 
-def check20(n):
-    if n % 20 == 0:
-        return check19(n)
+def check9(n):
+    if n % 9 == 0:
+        return check8(n)
     return False
 
 
-def check19(n):
-    if n % 19 == 0:
-        return check18(n)
+def check8(n):
+    if n % 8 == 0:
+        return check7(n)
     return False
 
 
-def check18(n):
-    if n % 18 == 0:
-        return check17(n)
+def check7(n):
+    if n % 7 == 0:
+        return check6(n)
     return False
 
 
-def check17(n):
-    if n % 17 == 0:
-        return check16(n)
+def check6(n):
+    if n % 6 == 0:
+        return check5(n)
     return False
 
 
-def check16(n):
-    if n % 16 == 0:
-        return check15(n)
+def check5(n):
+    if n % 5 == 0:
+        return check4(n)
     return False
 
 
-def check15(n):
-    if n % 15 == 0:
-        return check14(n)
+def check4(n):
+    if n % 4 == 0:
+        return True
     return False
 
 
@@ -61,13 +63,42 @@ def check11(n):
     return False
 
 
-lim1 = i = 1860480
-lim2 = 360360
-product = lim1 * lim2
-flag = 0
-while flag != 1 and i < product:
-    if check20(i):
-        print(i)
-        flag = 1
-    print(i)
-    i += 1
+# Factors - 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
+# Unique Factors - 5, 7, 9, 11, 13, 16, 17, 19
+# Lowest Multiple - 232792560
+
+# Not optimal.
+# factors = [19, 17, 16, 13, 11, 9, 7, 5]
+# lim1 = i = math.factorial(20)
+# i = 323323
+# flag = 0
+# while flag != 8:
+#     for j in factors:
+#         if i % j:
+#
+#             flag += 1
+#     i += 323323
+# print(i)
+
+# TODO: I need to find Unique factors for myself and multiply them to get the lowest multiple
+
+# Finding unique factors of 10 - 9, 8, 7, 5
+factorial10 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+for i in factorial10:
+    j = 2
+    count = 0
+    limit = i / 2
+    temp = 0
+    factor = []
+    while j <= limit:
+        if i % j == 0:
+            count += 1
+            factor.append(j)
+        j += 1
+    if count > 1 and:
+        factorial10.remove(i)
+    if count == 1:
+        factorial10.remove(temp)
+
+print(factorial10)
