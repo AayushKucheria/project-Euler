83eff69f8050a93ed7d761e8a87ecf4aca65f8db
Aayush Kucheria <aayush.kucheria@gmail.com>
2019-03-21 13:12:29 +0530

    Working on 5

--- 5.py
+++ 5.py
@@ -1,104 +1,43 @@
-import math
+# Q -
 
-9, 9, 7, 6, 5, 4, 14, 13, 12, 11,
+# TRY - Multiply all prime nos in list and then check for factor
 
-
-def check9(n):
-    if n % 9 == 0:
-        return check8(n)
-    return False
-
-
-def check8(n):
-    if n % 8 == 0:
-        return check7(n)
-    return False
-
-
-def check7(n):
-    if n % 7 == 0:
-        return check6(n)
-    return False
+def getprime(original):
+    prime = []
+    flag = 0
+    for i in original:
+        for j in range(2, i):
+            if i % j == 0:
+                flag = 1
+                break
+        if flag == 0:
+            prime.append(i)
+    return prime
 
 
-def check6(n):
-    if n % 6 == 0:
-        return check5(n)
-    return False
+print("Enter the higher limit - ")
+limit = int(input())
+list = [1]
+for i in range(2, limit + 1):
+    list.append(i)
+print("Original List: ", list)
+primelist = getprime(list)
+print("Prime list: ", primelist)
 
-
-def check5(n):
-    if n % 5 == 0:
-        return check4(n)
-    return False
-
-
-def check4(n):
-    if n % 4 == 0:
-        return True
-    return False
-
-
-def check14(n):
-    if n % 14 == 0:
-        return check13(n)
-    return False
-
-
-def check13(n):
-    if n % 13 == 0:
-        return check12(n)
-    return False
-
-
-def check12(n):
-    if n % 12 == 0:
-        return check11(n)
-    return False
-
-
-def check11(n):
-    if n % 11 == 0:
-        return True
-    return False
-
-
-# Factors - 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
-# Unique Factors - 5, 7, 9, 11, 13, 16, 17, 19
-# Lowest Multiple - 232792560
-
-# Not optimal.
-# factors = [19, 17, 16, 13, 11, 9, 7, 5]
-# lim1 = i = math.factorial(20)
-# i = 323323
-# flag = 0
-# while flag != 8:
-#     for j in factors:
-#         if i % j:
-#
-#             flag += 1
-#     i += 323323
-# print(i)
-
-# TODO: I need to find Unique factors for myself and multiply them to get the lowest multiple
-
-# Finding unique factors of 10 - 9, 8, 7, 5
-factorial10 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-
-for i in factorial10:
-    j = 2
-    count = 0
-    limit = i / 2
-    temp = 0
-    factor = []
-    while j <= limit:
-        if i % j == 0:
-            count += 1
-            factor.append(j)
-        j += 1
-    if count > 1 and:
-        factorial10.remove(i)
-    if count == 1:
-        factorial10.remove(temp)
-
-print(factorial10)
+# primeproduct = 19 * 17 * 13 * 11 * 7 * 5 * 3 * 2
+# print("prime product is ", primeproduct)
+# print("24 times primeproduct is ", 24 * primeproduct)
+# listwithoutprime = [20, 18, 16, 15, 14, 12, 10, 9, 8, 6, 4]
+# count = 0
+# while count != 11:
+#     count = 0
+#     for i in listwithoutprime:
+#         if primeproduct % i == 0:
+#             count += 1
+#             print(count)
+#         else:
+#             break
+#     if count != 11:
+#         primeproduct += 9699690
+#     else:
+#         print(primeproduct)
